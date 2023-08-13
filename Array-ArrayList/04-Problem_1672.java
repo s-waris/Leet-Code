315a2bb41da40114639f5c201fa7eb177eabb21b
Saad Waris <sayhellotosaad@gmail.com>
2023-08-13 23:36:32 +0530

    Richest Customer Wealth

---
+++ Array-ArrayList/04-Problem_1672.java
@@ -0,0 +1,17 @@
+//Leetcode Problem 1672
+//Richest Customer Wealth : https://leetcode.com/problems/richest-customer-wealth
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for(int i = 0; i<accounts.length; i++){
+            int temp = 0;
+            for(int j = 0; j<accounts[i].length; j++){
+                temp+=accounts[i][j];
+            }
+            if(temp>max)
+                max=temp;
+        }
+        return max;
+    }
+}
---
+++ Array-ArrayList/05-Problem_1470.java
@@ -0,0 +1,14 @@
+// LeetCode Problem 1480
+// Shuffle the Array : https://leetcode.com/problems/shuffle-the-array
+
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2*n];
+        for(int i = 0; i<n; i++){
+            ans[i*2]=nums[i];
+            ans[i*2+1]=nums[n+i];
+        }
+        return ans;
+    }
+}
