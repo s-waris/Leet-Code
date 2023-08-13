778beb4d214a701026d4af33975fc92e83152989
Saad Waris <sayhellotosaad@gmail.com>
2023-08-13 23:39:31 +0530

    Shuffle the Array

--- Array-ArrayList/05-Problem_1470.java
+++ Array-ArrayList/05-Problem_1470.java
@@ -5,7 +5,8 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] ans = new int[2*n];
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<n; i++)
+        {
             ans[i*2]=nums[i];
             ans[i*2+1]=nums[n+i];
         }
