1e02b4dc16c01fb5023e800d0aebb25ae6d9068e
Saad Waris <sayhellotosaad@gmail.com>
2023-08-15 23:16:35 +0530

    Number of good Pairs

---
+++ Array-ArrayList/07-Problem_1512.java
@@ -0,0 +1,15 @@
+// LeetCode Problem 1512
+// Number of Good Pairs : https://leetcode.com/problems/number-of-good-pairs
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int good = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]==nums[j])
+                    good++;
+            }
+        }
+        return good;
+    }
+}
