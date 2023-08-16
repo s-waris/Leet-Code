1a00541db6bbabe2af23fe6c8c20da4dfacccb13
Saad Waris <sayhellotosaad@gmail.com>
2023-08-16 20:24:57 +0530

    Check if the Sentence Is Pangram

---
+++ Array-ArrayList/09-Problem_1832.java
@@ -0,0 +1,19 @@
+// LeetCode Problem 1832
+// Check if the Sentence Is Pangram : https://leetcode.com/problems/check-if-the-sentence-is-pangram
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean flag = true;
+        int[] arr = new int[26];
+        for(int i=0; i<sentence.length(); i++){
+            arr[sentence.charAt(i)-'a']++;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i]==0){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
