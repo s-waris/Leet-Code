be3f8cbee5c99762d4a20e4ac4fe20bf3df6b3ff
Saad Waris <sayhellotosaad@gmail.com>
2023-09-03 22:54:59 +0530

    Maximum Subarray

---
+++ Array-ArrayList/24-Problem_0053.java
@@ -0,0 +1,47 @@
+// LeetCode Problem 0053
+// Maximum Subarray : https://leetcode.com/problems/maximum-subarray
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int ans=nums[0]; int oldMax=ans;
+        int max=ans, min=ans, minIn=0, maxIn=0, start=0;
+        int end=nums.length-1;
+        while(true){
+            int sum=0;
+            for(int i=start; i<=end; i++){
+                if(sum<0){
+                    i--;
+                    sum=0;
+                } 
+                else{
+                    sum=sum+nums[i];
+                    if(sum>=max){
+                        max=sum;
+                        maxIn=i;
+                    }
+                    if(sum<=min){
+                        min=sum;
+                        minIn=i;
+                    }
+                }
+            }
+            if(minIn>maxIn)
+                end=maxIn;
+            else if(min<maxIn){
+                start=minIn+1;
+                end=maxIn;
+            }
+            else if(minIn==maxIn){
+                ans=max;
+                break;
+            }
+            if(oldMax>=max){
+                ans=max;
+                break;
+            }
+            else
+                oldMax=max;
+        }
+        return ans;
+    }
+}
