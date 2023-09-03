24779714013e5ed6f12b70c1e3364a03c822cf7f
Saad Waris <sayhellotosaad@gmail.com>
2023-09-04 00:02:54 +0530

    Maximum Subarray

--- Array-ArrayList/24-Problem_0053.java
+++ Array-ArrayList/24-Problem_0053.java
@@ -9,20 +9,17 @@ class Solution {
         while(true){
             int sum=0;
             for(int i=start; i<=end; i++){
-                if(sum<0){
-                    i--;
+                if(sum<0)
                     sum=0;
-                } 
-                else{
-                    sum=sum+nums[i];
-                    if(sum>=max){
-                        max=sum;
-                        maxIn=i;
-                    }
-                    if(sum<=min){
-                        min=sum;
-                        minIn=i;
-                    }
+                sum=sum+nums[i];
+                if(sum>=max){
+                    max=sum;
+                    maxIn=i;
+                }
+                if(sum<=min){
+                    min=sum;
+                    minIn=i;
+                
                 }
             }
             if(minIn>maxIn)
