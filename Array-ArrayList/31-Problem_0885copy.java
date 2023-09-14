44eb54e64f20513f42a45acfd1170cc80f45e1ce
Saad Waris <sayhellotosaad@gmail.com>
2023-09-14 14:51:28 +0530

    Spiral Matrix III

--- Array-ArrayList/31-Problem_0885copy.java
+++ Array-ArrayList/31-Problem_0885copy.java
@@ -8,196 +8,196 @@ class Solution {
 
   public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
     int[][] ans = new int[rows*cols][2];
-      right(rows, cols, rStart, cStart, rStart, cStart+1, ans);
-      return ans;
-    }
+    right(rows, cols, rStart, cStart, rStart, cStart+1, ans);
+    return ans;
+  }
 
-    public void right(int rows, int cols, int rStart, int cStart, int rEnd, int cEnd, int[][] ans){
-      if(key<4){
-        if(rStart>-1){
-          key=0;
-          if(cStart>-1 && cEnd<cols){
-            for(int i=cStart; i<cEnd; i++){
+  public void right(int rows, int cols, int rStart, int cStart, int rEnd, int cEnd, int[][] ans){
+    if(key<4){
+      if(rStart>-1){
+        key=0;
+        if(cStart>-1 && cEnd<cols){
+          for(int i=cStart; i<cEnd; i++){
+            ans[n][0]=rStart;
+            ans[n++][1]=i;
+          }
+        }
+        else if (cStart<0){
+          if(cEnd<cols){
+            for(int i=0; i<cEnd; i++){
               ans[n][0]=rStart;
               ans[n++][1]=i;
             }
           }
-          else if (cStart<0){
-            if(cEnd<cols){
-              for(int i=0; i<cEnd; i++){
-                ans[n][0]=rStart;
-                ans[n++][1]=i;
-              }
-            }
-            else{
-              for(int i=0; i<cols; i++){
-                ans[n][0]=rStart;
-                ans[n++][1]=i;
+          else{
+            for(int i=0; i<cols; i++){
+              ans[n][0]=rStart;
+              ans[n++][1]=i;
             }
           }
-          }
-          else if(cEnd>=cols){
-            if(cStart<0){
-              for(int i=0; i<cols; i++){
-                ans[n][0]=rStart;
-                ans[n++][1]=i;
-              }
-            }
-            else{
-              for(int i=cStart; i<cols; i++){
-                ans[n][0]=rStart;
-                ans[n++][1]=i;
-            }
-          }
-          }
-          down(rows, cols, rStart, cStart, ++rEnd, cEnd, ans);
         }
-        else{
-          key++;
-          down(rows, cols, rStart, cStart, ++rEnd, cEnd, ans);
+        else if(cEnd>=cols){
+          if(cStart<0){
+            for(int i=0; i<cols; i++){
+              ans[n][0]=rStart;
+              ans[n++][1]=i;
+            }
+          }
+          else{
+            for(int i=cStart; i<cols; i++){
+              ans[n][0]=rStart;
+              ans[n++][1]=i;
+            }
+          }
         }
+        down(rows, cols, rStart, cStart, ++rEnd, cEnd, ans);
       }
-    }
-
-    public void down(int rows, int cols, int rStart, int cStart, int rEnd, int cEnd, int[][] ans){
-      if(key<4){
-        if(cEnd<cols){
-          key=0;
-          if(rStart>-1 && rEnd<rows){
-            for(int i=rStart; i<rEnd; i++){
-              ans[n][0]=i;
-              ans[n++][1]=cEnd;
-            }
-          }
-          else if(rStart<0){
-            if(rEnd>rows){
-              for(int i=0; i<rows; i++){
-              ans[n][0]=i;
-              ans[n++][1]=cEnd;
-            }
-            }
-            else{
-              for(int i=0; i<rEnd; i++){
-              ans[n][0]=i;
-              ans[n++][1]=cEnd;
-            }
-          }
-          }
-        
-          else if(rEnd>=rows){
-            if(rStart<0){
-              for(int i=0; i<rows; i++){
-              ans[n][0]=i;
-              ans[n++][1]=cEnd;
-            }
-            }
-            else{
-              for(int i=rStart; i<rows; i++){
-              ans[n][0]=i;
-              ans[n++][1]=cEnd;
-            }
-          }
-        }
-          left(rows, cols, rStart, --cStart, rEnd, cEnd, ans);
-        }
-        else{
-          key++;
-          left(rows, cols, rStart, --cStart, rEnd, cEnd, ans);
-        }
-      }
-    }
-
-    public void left(int rows, int cols, int rStart, int cStart, int rEnd, int cEnd, int[][] ans){
-      if(key<4){
-        if(rEnd<rows){
-          key=0;
-          if(cStart>-1 && cEnd<cols){
-            for(int i=cEnd; i>cStart; i--){
-              ans[n][0]=rEnd;
-              ans[n++][1]=i;
-            }
-          }
-          else if(cStart<0){
-            if(cEnd>=cols){
-              for(int i=cols-1; i>=0; i--){
-              ans[n][0]=rEnd;
-              ans[n++][1]=i;
-            }
-            }
-            else{
-              for(int i=cEnd; i>=0; i--){
-              ans[n][0]=rEnd;
-              ans[n++][1]=i;
-            }
-          }
-          }
-          else if(cEnd>=cols){
-            if(cStart<0){
-              for(int i=cols-1; i>=0; i--){
-              ans[n][0]=rEnd;
-              ans[n++][1]=i;
-            }
-        }
-            else{
-              for(int i=cols-1; i>cStart; i--){
-              ans[n][0]=rEnd;
-              ans[n++][1]=i;
-            }
-          }
-        }
-          up(rows, cols, --rStart, cStart, rEnd, cEnd, ans);
-        }
-        else{
-          key++;
-          up(rows, cols, --rStart, cStart, rEnd, cEnd, ans);
-        }
-      }
-    }
-
-    public void up(int rows, int cols, int rStart, int cStart, int rEnd, int cEnd, int[][] ans){
-      if(key<4){
-        if(cStart>-1){
-          key=0;
-          if(rStart>-1 && rEnd<rows){
-            for(int i=rEnd; i>rStart; i--){
-              ans[n][0]=i;
-              ans[n++][1]=cStart;
-            }}
-          else if(rStart<0){
-            if(rEnd>=rows){
-                for(int i=rows-1; i>=0; i--){
-                    ans[n][0]=i;
-                    ans[n++][1]=cStart;
-                }
-            }
-            else{
-                for(int i=rEnd; i>=0; i--){
-                    ans[n][0]=i;
-                    ans[n++][1]=cStart;
-                }
-            }
-          }
-          else if(rEnd>=rows){
-            if(rStart<0){
-                for(int i=rows-1; i>=0; i--){
-                    ans[n][0]=i;
-                    ans[n++][1]=cStart;
-                }
-            }
-            else{
-                for(int i=rows-1; i>rStart; i--){
-                    ans[n][0]=i;
-                    ans[n++][1]=cStart;
-                }
-            }
-          }
-          }
-          
-          right(rows, cols, rStart, cStart, rEnd, ++cEnd, ans);
-        }
       else{
         key++;
-        right(rows, cols, rStart, cStart, rEnd, ++cEnd, ans);
+        down(rows, cols, rStart, cStart, ++rEnd, cEnd, ans);
       }
     }
+  }
+
+  public void down(int rows, int cols, int rStart, int cStart, int rEnd, int cEnd, int[][] ans){
+    if(key<4){
+      if(cEnd<cols){
+        key=0;
+        if(rStart>-1 && rEnd<rows){
+          for(int i=rStart; i<rEnd; i++){
+            ans[n][0]=i;
+            ans[n++][1]=cEnd;
+          }
+        }
+        else if(rStart<0){
+          if(rEnd>rows){
+            for(int i=0; i<rows; i++){
+              ans[n][0]=i;
+              ans[n++][1]=cEnd;
+            }
+          }
+          else{
+            for(int i=0; i<rEnd; i++){
+              ans[n][0]=i;
+              ans[n++][1]=cEnd;
+            }
+          }
+        }
+        
+        else if(rEnd>=rows){
+          if(rStart<0){
+            for(int i=0; i<rows; i++){
+              ans[n][0]=i;
+              ans[n++][1]=cEnd;
+            }
+          }
+          else{
+            for(int i=rStart; i<rows; i++){
+              ans[n][0]=i;
+              ans[n++][1]=cEnd;
+            }
+          }
+        }
+        left(rows, cols, rStart, --cStart, rEnd, cEnd, ans);
+      }
+      else{
+        key++;
+        left(rows, cols, rStart, --cStart, rEnd, cEnd, ans);
+      }
+    }
+  }
+
+  public void left(int rows, int cols, int rStart, int cStart, int rEnd, int cEnd, int[][] ans){
+    if(key<4){
+      if(rEnd<rows){
+        key=0;
+        if(cStart>-1 && cEnd<cols){
+          for(int i=cEnd; i>cStart; i--){
+            ans[n][0]=rEnd;
+            ans[n++][1]=i;
+          }
+        }
+        else if(cStart<0){
+          if(cEnd>=cols){
+            for(int i=cols-1; i>=0; i--){
+              ans[n][0]=rEnd;
+              ans[n++][1]=i;
+            }
+          }
+          else{
+            for(int i=cEnd; i>=0; i--){
+              ans[n][0]=rEnd;
+              ans[n++][1]=i;
+            }
+          }
+        }
+        else if(cEnd>=cols){
+          if(cStart<0){
+            for(int i=cols-1; i>=0; i--){
+              ans[n][0]=rEnd;
+              ans[n++][1]=i;
+            }
+          }
+          else{
+            for(int i=cols-1; i>cStart; i--){
+              ans[n][0]=rEnd;
+              ans[n++][1]=i;
+            }
+          }
+        }
+        up(rows, cols, --rStart, cStart, rEnd, cEnd, ans);
+      }
+      else{
+        key++;
+        up(rows, cols, --rStart, cStart, rEnd, cEnd, ans);
+      }
+    }
+  }
+
+  public void up(int rows, int cols, int rStart, int cStart, int rEnd, int cEnd, int[][] ans){
+    if(key<4){
+      if(cStart>-1){
+        key=0;
+        if(rStart>-1 && rEnd<rows){
+          for(int i=rEnd; i>rStart; i--){
+              ans[n][0]=i;
+              ans[n++][1]=cStart;
+          }
+        }
+        else if(rStart<0){
+          if(rEnd>=rows){
+            for(int i=rows-1; i>=0; i--){
+              ans[n][0]=i;
+              ans[n++][1]=cStart;
+            }
+          }
+          else{
+            for(int i=rEnd; i>=0; i--){
+              ans[n][0]=i;
+              ans[n++][1]=cStart;
+            }
+          }
+        }
+        else if(rEnd>=rows){
+          if(rStart<0){
+            for(int i=rows-1; i>=0; i--){
+              ans[n][0]=i;
+              ans[n++][1]=cStart;
+            }
+          }
+          else{
+            for(int i=rows-1; i>rStart; i--){
+              ans[n][0]=i;
+              ans[n++][1]=cStart;
+            }
+          }
+        }
+      }
+      right(rows, cols, rStart, cStart, rEnd, ++cEnd, ans);
+    }
+    else{
+      key++;
+      right(rows, cols, rStart, cStart, rEnd, ++cEnd, ans);
+    }
+  }
 }
