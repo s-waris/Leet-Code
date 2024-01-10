796aefc155104d339d0e621ace7aa410ee7a8ceb
Saad Waris <sayhellotosaad@gmail.com>
2024-01-10 23:51:50 +0530

    Search Insert Position

---
+++ Searching/40-Problem_0035.java
@@ -0,0 +1,24 @@
+//Leetcode Problem 0035
+//Search Insert Position : https://leetcode.com/problems/search-insert-position
+
+  class Solution {
+    public int searchInsert(int[] nums, int target) {
+      int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target)
+                return m;
+
+            
+            if (nums[m] < target)
+                l = m + 1;
+
+            else
+                r = m - 1;
+        }
+
+        return l;
+
+    }
+}
+
